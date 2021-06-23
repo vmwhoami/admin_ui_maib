db7f8250fdf9c24e3c740687a773c3b1e56d9ccd
Vitalie Melnic <vmwhoami@gmail.com>
2021-06-23 17:36:58 +0200

    Add the sidebar with two buttons

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,9 +1,10 @@
 import React, { useEffect } from 'react';
-import { Container, Row } from 'react-bootstrap';
+import { Container, Row, Col } from 'react-bootstrap';
 import PropTypes from 'prop-types';
 import { useDispatch } from 'react-redux';
 import getUsers from '../redux/reducer/actions';
 import NavBar from './navbar/NavBar';
+import LeftBar from './leftBar/LeftBar';
 
 const Layout = ({ children }) => {
   const dispatch = useDispatch();
@@ -13,8 +14,11 @@ const Layout = ({ children }) => {
   return (
     <Container className="px-0" fluid>
       <NavBar />
-      <Row>
-        {children}
+      <Row className="w-100">
+        <LeftBar />
+        <Col xs={9}>
+          {children}
+        </Col>
       </Row>
     </Container>
   );
--- src/components/leftBar/LeftBar.js
+++ src/components/leftBar/LeftBar.js
@@ -1,10 +1,18 @@
 import React from 'react';
 import { Col, Button } from 'react-bootstrap';
+import { AiOutlineDashboard } from 'react-icons/ai';
+import { GrSupport } from 'react-icons/gr';
 
 const LeftBar = () => (
-  <Col>
-    <Button>Dashboard</Button>
-    <Button>Support</Button>
+  <Col className="left-bar d-flex flex-column ">
+    <Button variant="light" className="d-flex h-2 align-items-center ml-4 my-3 fs-4 cust-button">
+      <AiOutlineDashboard className="mr-2" />
+      Dashboard
+    </Button>
+    <Button variant="light" className="d-flex h-2 align-items-center  ml-4 fs-4 cust-button">
+      <GrSupport className="mr-2 text-white" />
+      Support
+    </Button>
   </Col>
 );
 
