14203a5e2055c4b032550d896820674c26506016
Vitalie Melnic <vmwhoami@gmail.com>
2021-06-24 14:47:53 +0200

    Add findUser form fields values and handle change event

--- src/components/FindUser/FindUser.js
+++ src/components/FindUser/FindUser.js
@@ -1,35 +1,70 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {
   Form, Row, Col, Button,
 } from 'react-bootstrap';
 
-const FindUser = () => (
-  <>
-    <h4>Cauta Uilizator</h4>
-    <Form as={Row}>
-      <Col lg={8}>
-        <Row className="mb-3 d-flex align-items-end">
-          <Form.Group as={Col} controlId="formGridCity">
-            <Form.Label className="green">IDNP</Form.Label>
-            <Form.Control className="rounded-0 border-top-0 border-left-0 border-right-0 shadow-none mt-2 px-0" type="text" placeholder="nr IDNP" />
-          </Form.Group>
+const FindUser = () => {
+  const [fields, setFields] = useState({
+    idnp: '',
+    phone: '',
+    date: '',
+  });
+  const handleChange = e => {
+    const { name, value } = e.target;
+    setFields({
+      ...fields,
+      [name]: value,
+    });
+  };
 
-          <Form.Group as={Col} controlId="formGridState">
-            <Form.Label className="green">Nr. Telefon</Form.Label>
-            <Form.Control className="rounded-0 border-top-0 border-left-0 border-right-0 shadow-none mt-2 px-0" type="text" placeholder="Nr.Telefon" />
-          </Form.Group>
+  return (
+    <>
+      <h4>Cauta Uilizator</h4>
+      <Form as={Row}>
+        <Col lg={8}>
+          <Row className="mb-3 d-flex align-items-end">
+            <Form.Group as={Col} controlId="formGridCity">
+              <Form.Label className="green">IDNP</Form.Label>
+              <Form.Control
+                name="idnp"
+                onChange={handleChange}
+                value={fields.idnp}
+                className="rounded-0 border-top-0 border-left-0 border-right-0 shadow-none mt-2 px-0"
+                type="text"
+                placeholder="nr IDNP"
+              />
+            </Form.Group>
 
-          <Form.Group as={Col} controlId="formGridZip">
-            <Form.Label className="green">Data acces</Form.Label>
-            <Form.Control className="rounded-0 border-top-0 border-left-0 border-right-0 shadow-none mt-2 px-0" type="date" />
-          </Form.Group>
-          <Form.Group as={Col} controlId="formGridZip">
-            <Button className="w-100 rounded-0 title text-uppercase border-0">Cauta</Button>
-          </Form.Group>
-        </Row>
-      </Col>
-    </Form>
-  </>
-);
+            <Form.Group as={Col} controlId="formGridState">
+              <Form.Label className="green">Nr. Telefon</Form.Label>
+              <Form.Control
+                name="phone"
+                onChange={handleChange}
+                value={fields.phone}
+                className="rounded-0 border-top-0 border-left-0 border-right-0 shadow-none mt-2 px-0"
+                type="text"
+                placeholder="Nr.Telefon"
+              />
+            </Form.Group>
+
+            <Form.Group as={Col} controlId="formGridZip">
+              <Form.Label className="green">Data acces</Form.Label>
+              <Form.Control
+                name="date"
+                onChange={handleChange}
+                value={fields.date}
+                className="rounded-0 border-top-0 border-left-0 border-right-0 shadow-none mt-2 px-0"
+                type="date"
+              />
+            </Form.Group>
+            <Form.Group as={Col} controlId="formGridZip">
+              <Button className="w-100 rounded-0 title text-uppercase border-0">Cauta</Button>
+            </Form.Group>
+          </Row>
+        </Col>
+      </Form>
+    </>
+  );
+};
 
 export default FindUser;
