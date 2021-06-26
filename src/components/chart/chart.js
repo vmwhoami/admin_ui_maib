8859213adc86cc7ddc53ecda4597bc3c6f325cdb
Vitalie Melnic <vmwhoami@gmail.com>
2021-06-26 22:16:52 +0200

    Install react-chartjs-2

--- src/components/chart/chart.js
+++ src/components/chart/chart.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Row } from 'react-bootstrap';
 import { useSelector } from 'react-redux';
+import { Line } from 'react-chartjs-2';
 
 const Chart = () => {
   const downloads = useSelector(state => state.downloads.downloads);
