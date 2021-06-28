8f34ad887fb720bcaf38e02677bd1ceb1bac4b23
Vitalie Melnic <vmwhoami@gmail.com>
2021-06-28 22:38:45 +0300

    Add report columns

--- src/components/report/report.js
+++ src/components/report/report.js
@@ -1,11 +1,13 @@
-import React from 'react'
+import React from 'react';
+import { Row, Col } from 'react-bootstrap';
 
-const Report = () => {
-  return (
-    <div>
+const Report = () => (
+  <Row className="border-bottom pb-2 ml-1">
+    <Col><h6>Data</h6></Col>
+    <Col><h6>Numar utilizatori total</h6></Col>
+    <Col><h6>Numar utilizatori unici</h6></Col>
+    <Col><h6>Numar utilizatori unici</h6></Col>
+  </Row>
+);
 
-    </div>
-  )
-}
-
-export default Report
+export default Report;
